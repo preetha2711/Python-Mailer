f15419d56222b2064e4c6f256b965a0e0393b3db
Preetha Datta <datta.preetha@gmail.com>
2018-03-11 16:27:46 +0530

    Mailer
    Uses SMTP module

---
+++ mailer.py
@@ -0,0 +1,21 @@
+import smtplib
+
+#tester
+content = "python mailer test"
+receiver = 'reeju.datta@gmail.com'
+email = 'datta.preetha@gmail.com'
+password = 'vincentcrabbe'
+
+
+
+for i in range(0,10):
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+    mail.ehlo()
+
+    mail.starttls()
+    mail.login(email,password)
+    fromemail = email
+    mail.sendmail(fromemail,receiver,content)
+
+    mail.close()
