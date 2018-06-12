e099f8ea429563de7d4f5a82453bd6ae4d3f3c2a
Preetha Datta <datta.preetha@gmail.com>
2018-06-12 19:14:15 +0530

    Update tester.php

--- tester.php
+++ tester.php
@@ -1,6 +1,5 @@
 <?php
-
-class CashfreePaymentModuleFrontController extends ModuleFrontController {
+class PaytmPaymentModuleFrontController extends ModuleFrontController {
 	public $ssl = true;
 	
 	public function init() {
@@ -11,65 +10,39 @@ class CashfreePaymentModuleFrontController extends ModuleFrontController {
 		parent::initContent();
 		
 		global $smarty, $cart;
-
 		$bill_address = new Address(intval($cart->id_address_invoice));
 		$customer = new Customer(intval($cart->id_customer));
-
 		if (!Validate::isLoadedObject($bill_address) OR ! Validate::isLoadedObject($customer))
-			return $this->l("Cashfree error: (invalid address or customer)");
-		$secretKey = "secret_key";
-		$time = strtotime("now");
-		
+			return $this->l("Paytm error: (invalid address or customer)");
 		$order_id = intval($cart->id);
-		$customerName = strval($customer->firstname.' '.$customer->lastname);
-		$customerPhone = "0000000000"; //not sending data. 
-		$customerEmail = strval($customer->email);		
-		$returnUrl = "http://localhost:8888/en/module/cashfree/validation"; //allows redirection to the Cashfree page
-		$notifyUrl = "http://localhost:8888/en/module/cashfree/validation"; //allows redirection to the Cashfree page 
-
+		// $order_id = "RHL_" . strtotime("now") . "__" . $order_id; // just for testing
 		$amount = $cart->getOrderTotal(true, Cart::BOTH);
-		$orderCurrency = "INR";
-		$orderNote = "PrestaShop order";
-
-		$appId = Configuration::get("Cashfree_MERCHANT_ID");
-		
-		$secretKey = Configuration::get("Cashfree_MERCHANT_KEY");
-		
-		$secretKey = "2279c0ffb9550ad0f9e0652741c8d06a49409517";
-		
-		$postData = array( 
-		"appId" => $appId, 
-		"orderId" => $order_id, 
-		"orderAmount" => $amount,
-		"orderCurrency" => $orderCurrency, 
-		"customerName" => $customerName, 
-		"customerPhone" => $customerPhone, 
-		"customerEmail" => $customerEmail,
-		"returnUrl" => $returnUrl,
-		"notifyUrl" => $notifyUrl, 
-		);	
-
-		ksort($postData);
-		$signatureData = "";
-		foreach ($postData as $key => $value){
-			 $signatureData .= $key.$value;
-		}
-		$signature = hash_hmac('sha256', $signatureData, $secretKey,true);
-		$signature = base64_encode($signature);
-	   
-		$postData["signature"] = $signature;
-		
-		$post_variables = $postData;
-
-
+		$post_variables = array(
+			"MID" => Configuration::get("Paytm_MERCHANT_ID"),
+			"ORDER_ID" => $order_id,
+			"CUST_ID" => intval($cart->id_customer),
+			"TXN_AMOUNT" => $amount,
+			"CHANNEL_ID" => Configuration::get("Paytm_MERCHANT_CHANNEL_ID"),
+			"INDUSTRY_TYPE_ID" => Configuration::get("Paytm_MERCHANT_INDUSTRY_TYPE"),
+			"WEBSITE" => Configuration::get("Paytm_MERCHANT_WEBSITE"),
+		);
+		if(isset($bill_address->phone_mobile) && trim($bill_address->phone_mobile) != "")
+			$post_variables["MOBILE_NO"] = preg_replace("#[^0-9]{0,13}#is", "", $bill_address->phone_mobile);
+		if(isset($customer->email) && trim($customer->email) != "")
+			$post_variables["EMAIL"] = $customer->email;
+		if (Configuration::get("Paytm_CALLBACK_URL_STATUS") == "0")
+			$post_variables["CALLBACK_URL"] = $this->module->getDefaultCallbackUrl();
+		else
+			$post_variables["CALLBACK_URL"] = Configuration::get("Paytm_CALLBACK_URL");
+		$post_variables["CHECKSUMHASH"] = getChecksumFromArray($post_variables, Configuration::get("Paytm_MERCHANT_KEY"));
 		$smarty->assign(
 						array(
-							"cashfree_post" => $post_variables,
-							"action" => Configuration::get("Cashfree_GATEWAY_URL")
+							"paytm_post" => $post_variables,
+							"action" => Configuration::get("Paytm_GATEWAY_URL")
 							)
 					);
 		
-		// return $this->display(__FILE__, 'cashfree.tpl');
-		$this->setTemplate('module:cashfree/views/templates/front/payment_form.tpl');
+		// return $this->display(__FILE__, 'paytm.tpl');
+		$this->setTemplate('module:paytm/views/templates/front/payment_form.tpl');
 	}
 }
